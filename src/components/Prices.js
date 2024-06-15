5edcf2f1bc0ea017f8472cb028e2fe34236ad95a
Khush savaliya <savaliyakhush82012@gmail.com>
2024-06-15 15:20:11 +0530

    committed

---
+++ src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "Rs.0 to 499",
+    array: [0, 499],
+  },
+  {
+    _id: 1,
+    name: "Rs.500 to 999",
+    array: [500, 999],
+  },
+  {
+    _id: 2,
+    name: "Rs.1000 to 1799",
+    array: [1000, 1799],
+  },
+  {
+    _id: 3,
+    name: "Rs.1800 to 2499",
+    array: [1800, 2499],
+  },
+  {
+    _id: 4,
+    name: "Rs.2500 to 4999",
+    array: [2500, 4999],
+  },
+  {
+    _id: 4,
+    name: "Rs.5000 or more",
+    array: [5000, 99999],
+  },
+];
